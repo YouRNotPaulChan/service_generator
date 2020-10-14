d68b1bcbc3560646f6d5ead920903fa0968b9e8e
PC20 <15816170703@163.com>
2020-10-14 15:06:58 +0800

    :art: format indentation

--- lib/generators/rails/service/service_generator.rb
+++ lib/generators/rails/service/service_generator.rb
@@ -1,5 +1,6 @@
 require 'rails/generators'
 require 'rails/generators/generated_attribute'
+require 'byebug'
 
 module Rails
   module Generators
@@ -16,6 +17,53 @@ module Rails
 
       def module_name
         @module_name = options[:module]
+      end
+
+      def class_name
+        @class_name = file_name.classify
+      end
+
+      def class_body
+        if @module_name.present?
+          class_str.each_line.sum { |line| line.blank? ? line : "  #{line}" }
+        else
+          class_str
+        end
+      end
+
+      def class_str
+        <<~RUBY
+          class #{class_name} < ApplicationService
+            def initialize(attributes)
+              @attributes = attributes
+            end
+
+            def call
+              # some code
+            end
+            #{methods_body}
+          end
+        RUBY
+      end
+
+      def methods_body
+        return if methods.blank?
+
+        methods_str = methods.map do |method|
+          <<~RUBY
+            def #{method}
+              # some code
+            end
+          RUBY
+        end.join("\n")
+
+        private_str = <<~RUBY
+
+          private
+
+        RUBY
+        str = private_str + methods_str
+        str.each_line.sum { |line| line.blank? ? line : "  #{line}" }
       end
     end
   end
